935918ac38afd3e853b19b3cfb905bd2141c8266
hmz3510 <708926180@qq.com>
2020-07-14 10:30:01 +0800

    修改了代码

--- src/router/index.js
+++ src/router/index.js
@@ -41,6 +41,53 @@ export const constantRoutes = [
     path: '/404',
     component: () => import('@/views/404'),
     hidden: true
+  },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: 'Dashboard', icon: 'dashboard' }
+    }]
+  },
+
+  {
+    path: '/example',
+    component: Layout,
+    redirect: '/example/table',
+    name: 'Example',
+    meta: { title: 'Example', icon: 'example' },
+    children: [
+      {
+        path: 'table',
+        name: 'Table',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Table', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+
+  {
+    path: '/form',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Form',
+        component: () => import('@/views/form/index'),
+        meta: { title: 'Form', icon: 'form' }
+      }
+    ]
   }
 ]
 
@@ -50,66 +97,77 @@ export const constantRoutes = [
  */
 export const asyncRoutes = [
   {
-    path: '/monitorsObject',
+    path: '/nested',
     component: Layout,
-    redirect: '/monitorsObject/heartBeat',
-    name: 'monitorsObject',
+    redirect: '/nested/menu1',
+    name: 'Nested',
     meta: {
-      title: '监控对象配置',
-      icon: 'lock',
-      roles: ['ADMIN']
+      title: 'Nested',
+      icon: 'nested',
+      roles: ['admin']
     },
     children: [
       {
-        path: 'heartBeat',
-        component: () => import('@/views/monitorsObject/heartBeat'),
-        name: 'heartBeat',
-        meta: {
-          title: '设备心跳监控',
-          roles: ['ADMIN']
-        }
+        path: 'menu1',
+        component: () => import('@/views/nested/menu1/index'), // Parent router-view
+        name: 'Menu1',
+        meta: { title: 'Menu1' },
+        children: [
+          {
+            path: 'menu1-1',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'Menu1-1',
+            meta: { title: 'Menu1-1' }
+          },
+          {
+            path: 'menu1-2',
+            component: () => import('@/views/nested/menu1/menu1-2'),
+            name: 'Menu1-2',
+            meta: { title: 'Menu1-2' },
+            children: [
+              {
+                path: 'menu1-2-1',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+                name: 'Menu1-2-1',
+                meta: { title: 'Menu1-2-1' }
+              },
+              {
+                path: 'menu1-2-2',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+                name: 'Menu1-2-2',
+                meta: { title: 'Menu1-2-2' }
+              }
+            ]
+          },
+          {
+            path: 'menu1-3',
+            component: () => import('@/views/nested/menu1/menu1-3'),
+            name: 'Menu1-3',
+            meta: { title: 'Menu1-3' }
+          }
+        ]
       },
       {
-        path: 'service',
-        component: () => import('@/views/monitorsObject/service'),
-        name: 'service',
-        meta: {
-          title: '应用服务监控',
-          roles: ['ADMIN']
-        }
+        path: 'menu2',
+        component: () => import('@/views/nested/menu2/index'),
+        meta: { title: 'menu2' }
       }
     ]
   },
+
   {
-    path: '/add',
+    path: 'external-link',
     component: Layout,
-    name: 'addNewVue',
-    hidden: true,
-    meta: {
-      title: '监控对象配置'
-    },
     children: [
       {
-        path: 'newHeartBeat',
-        component: () => import('@/views/addNewVue/newHeartBeat'),
-        name: 'newHeartBeat',
-        meta: {
-          title: '新增监控对象'
-        }
-      },
-      {
-        path: 'newServer',
-        component: () => import('@/views/addNewVue/newServer'),
-        name: 'newServer',
-        meta: {
-          title: '新增监控对象'
-        }
+        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        meta: { title: 'External Link', icon: 'link' }
       }
     ]
   },
 
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/monitorsObject', hidden: true }
+  { path: '*', redirect: '/404', hidden: true }
 ]
 
 const createRouter = () => new Router({
